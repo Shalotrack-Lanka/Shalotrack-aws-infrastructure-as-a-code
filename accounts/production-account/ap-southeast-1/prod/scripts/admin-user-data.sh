b1fb294dbfee6432a7e737dbfe865eadb2fca06f
Amodamendis <amodarashmika2003@gmail.com>
2026-07-03 22:06:04 +0530

    modified admin-user-data.sh file with env variables

--- accounts/production-account/ap-southeast-1/prod/scripts/admin-user-data.sh
+++ accounts/production-account/ap-southeast-1/prod/scripts/admin-user-data.sh
@@ -2,4 +2,17 @@
 dnf update -y && dnf install -y docker
 systemctl enable docker && systemctl start docker
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${ecr_url}
-docker run -d --restart always -p 80:80 ${ecr_url}:latest
+
+
+docker run -d --restart always --name shalotrack-admin \
+  -p 80:80 \
+  -e APP_ENV="production" \
+  -e APP_DEBUG="false" \
+  -e APP_KEY="[YOUR_APP_KEY]" \
+  -e DB_CONNECTION="pgsql" \
+  -e DB_HOST="aws-0-ap-southeast-1.pooler.supabase.com" \
+  -e DB_PORT="6543" \
+  -e DB_DATABASE="postgres" \
+  -e DB_USERNAME="postgres.[YOUR_PROJECT]" \
+  -e DB_PASSWORD="[YOUR_PASSWORD]" \
+  ${ecr_url}:latest
