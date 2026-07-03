0b8520604c027b23feaaf1fb0dee32a3bdfbfcda
Amodamendis <amodarashmika2003@gmail.com>
2026-07-03 23:41:31 +0530

    modified gateway-user-data.sh with env variables

--- accounts/production-account/ap-southeast-1/prod/scripts/api-user-data.sh
+++ accounts/production-account/ap-southeast-1/prod/scripts/api-user-data.sh
@@ -2,4 +2,10 @@
 dnf update -y && dnf install -y docker
 systemctl enable docker && systemctl start docker
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${ecr_url}
-docker run -d --restart always -p 80:80 ${ecr_url}:latest
+
+docker run -d --restart always --name shalotrack-api \
+  -p 80:80 \
+  -e ASPNETCORE_HTTP_PORTS="80" \
+  -e ConnectionStrings__DefaultConnection="Host=aws-0-ap-southeast-1.pooler.supabase.com;Port=6543;Database=postgres;Username=postgres.[YOUR_PROJECT];Password=[YOUR_PASSWORD]" \
+  ${ecr_url}:latest
+
--- accounts/production-account/ap-southeast-1/prod/scripts/gateway-user-data.sh
+++ accounts/production-account/ap-southeast-1/prod/scripts/gateway-user-data.sh
@@ -2,4 +2,9 @@
 dnf update -y && dnf install -y docker
 systemctl enable docker && systemctl start docker
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${ecr_url}
-docker run -d --restart always -p 8000:8000 ${ecr_url}:latest
+
+docker run -d --restart always --name shalotrack-gateway \
+  -p 8000:9000 \
+  -e PORT="9000" \
+  -e DATABASE_URL="postgresql://postgres.riyjkfwxkamqbuuuwdli:tadK*7ASa_#,-NL@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres" \
+  ${ecr_url}:latest
