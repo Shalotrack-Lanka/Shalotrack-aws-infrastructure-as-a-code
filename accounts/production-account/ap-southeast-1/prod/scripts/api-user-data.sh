fa0e44e7e221f3e0a7dd2c70da329dff64ff1d61
Amodamendis <amodarashmika2003@gmail.com>
2026-07-07 20:10:02 +0530

    Updated api-user-data.sh to port 8080 bcz of Microsoft changed the default listening port for ASP.NET Core in .NET 8 from 80 to 8080

--- accounts/production-account/ap-southeast-1/prod/scripts/api-user-data.sh
+++ accounts/production-account/ap-southeast-1/prod/scripts/api-user-data.sh
@@ -5,7 +5,6 @@ systemctl enable docker && systemctl start docker
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${ecr_url}
 
 docker run -d --restart always --name shalotrack-api \
-  -p 80:80 \
-  -e ASPNETCORE_HTTP_PORTS="80" \
+  -p 80:8080 \
   -e ConnectionStrings__DefaultConnection="${db_connection_string}" \
   ${ecr_url}:latest
