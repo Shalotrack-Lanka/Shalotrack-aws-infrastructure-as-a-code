6ca58a356ec01f9ef94ab8058e31eda2bc945e0e
Amodamendis <amodarashmika2003@gmail.com>
2026-08-25 15:28:26 +0530

    Pool size chnaged in gateway & parameter called

--- accounts/production-account/ap-southeast-1/prod/scripts/gateway-user-data.sh
+++ accounts/production-account/ap-southeast-1/prod/scripts/gateway-user-data.sh
@@ -5,13 +5,15 @@ systemctl enable docker && systemctl start docker
 # 1. Authenticate with ECR
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${ecr_url}
 
-# 2. Dynamically pull production database secrets from AWS SSM parameter store
+# 2. Dynamically pull production secrets from AWS SSM parameter store
 export AWS_DEFAULT_REGION="ap-southeast-1"
 GATEWAY_DATABASE_URL=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/database_url" --with-decryption --query "Parameter.Value" --output text)
-GATEWAY_CONNECTION_TIMEOUT=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/connection_timeout" --with-decryption --query "Parameter.Value" --output text --region ap-southeast-1)
-GATEWAY_MAX_CONNECTIONS=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/max_connections" --with-decryption --query "Parameter.Value" --output text --region ap-southeast-1)
+GATEWAY_CONNECTION_TIMEOUT=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/connection_timeout" --query "Parameter.Value" --output text --region ap-southeast-1)
+GATEWAY_MAX_CONNECTIONS=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/max_connections" --query "Parameter.Value" --output text --region ap-southeast-1)
+GATEWAY_DB_POOL_MIN=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/db_pool_min" --query "Parameter.Value" --output text --region ap-southeast-1)
+GATEWAY_DB_POOL_MAX=$(aws ssm get-parameter --name "/shalotrack/prod/gateway/db_pool_max" --query "Parameter.Value" --output text --region ap-southeast-1)
 
-# 3. Spin up the TCP Gateway container, passing the secret securely from memory[cite: 3]
+# 3. Start the TCP Gateway container
 docker run -d --restart always --name shalotrack-gateway \
   -p 8000:9000 \
   -p 8001:9001 \
@@ -19,11 +21,12 @@ docker run -d --restart always --name shalotrack-gateway \
   -e DATABASE_URL="$GATEWAY_DATABASE_URL" \
   -e CONNECTION_TIMEOUT="$GATEWAY_CONNECTION_TIMEOUT" \
   -e MAX_CONNECTIONS="$GATEWAY_MAX_CONNECTIONS" \
+  -e DB_POOL_MIN="$GATEWAY_DB_POOL_MIN" \
+  -e DB_POOL_MAX="$GATEWAY_DB_POOL_MAX" \
   -e OTEL_EXPORTER_OTLP_ENDPOINT="http://otel.shalotrack.internal:4317" \
   ${ecr_url}:latest
 
-# 4. Node Exporter — exposes host-level CPU/RAM/Disk/Network metrics for Prometheus.
-# --net=host so it reports the real EC2 host's stats, not an isolated container's own.
+# 4. Node Exporter
 docker run -d --restart always --name node-exporter \
   --net="host" \
   --pid="host" \
